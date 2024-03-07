a7852f7f3ed9338d9dbb562ddd28e0953d916e96
wyn <k022298@gmail.com>
2024-03-07 19:22:00 +0000

    Initial Commit

---
+++ HotSeat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HotSeat
+//
+//  Created by wyn on 2024/3/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HotSeat/HotSeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotSeatApp.swift
+//  HotSeat
+//
+//  Created by wyn on 2024/3/7.
+//
+
+import SwiftUI
+
+@main
+struct HotSeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
