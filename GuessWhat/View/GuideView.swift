af746ffd7f7bfae1a2ff7aa1e85cf436681baa42
wyn <k022298@gmail.com>
2024-03-16 22:49:54 +0000

    Added GuideView, .guide state in game. Added a button in Game Setting view

---
+++ GuessWhat/View/GuideView.swift
@@ -0,0 +1,18 @@
+//
+//  GuideView.swift
+//  GuessWhat
+//
+//  Created by wyn on 2024/3/16.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuideView()
+}
